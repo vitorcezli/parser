f0ee194c86e4fbb887d3b73ad0180243d30cb5c4
Vítor Cézar <vitorcezli@gmail.com>
2017-08-15 09:58:54 -0300

    Added the complete function

--- earley.py
+++ earley.py
@@ -5,3 +5,24 @@ def addRuleOnChart(chart, position, rule):
 		chart[position] = [rule]
 	elif rule not in chart[position]:
 		chart[position].append(rule)
+
+
+def complete(rule):
+	return rule.index('.') == len(rule) - 1
+
+
+def earleyParse(sentence, grammar):
+	chart = {}
+	addRuleOnChart(chart, 0, ['start', '.', S])
+
+	
+
+
+chart = {}
+addRuleOnChart(chart, 0, ['test'])
+addRuleOnChart(chart, 0, ['test1'])
+addRuleOnChart(chart, 0, ['test'])
+addRuleOnChart(chart, 1, ['test'])
+print(chart)
+print(complete(['A', 'B', '.', 'C']))
+print(complete(['A', 'B', 'C', '.']))
