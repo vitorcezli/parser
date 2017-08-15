6e8e1e3a29b5a7442d0fd291fbfb00cf568a5ba2
Vítor Cézar <vitorcezli@gmail.com>
2017-08-15 09:28:00 -0300

    Earley parser implementation

---
+++ earley.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+def addRuleOnChart(chart, position, rule):
+	if position not in chart:
+		chart[position] = [rule]
+	elif rule not in chart[position]:
+		chart[position].append(rule)
